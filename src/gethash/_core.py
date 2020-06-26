986717225e4cbb9436ad625e9e4fcc905b3f210e
xymy <thyfan@163.com>
2020-06-26 17:04:13 +0800

    Add more control for progressbar

--- src/gethash/_core.py
+++ src/gethash/_core.py
@@ -26,9 +26,17 @@ def phash(hash_line: str) -> Tuple[bytes, str]:
     return bytes.fromhex(hash_value), path
 
 
-def gethash(ctx, path, *, chunk_size=DEFAULT_CHUNK_SIZE):
+def calcualte_hash(
+    ctx,
+    path: PathLike,
+    *,
+    chunk_size=DEFAULT_CHUNK_SIZE,
+    file=None,
+    disable=False
+) -> bytes:
     file_size = os.path.getsize(path)
-    bar = tqdm(total=file_size, leave=False, ascii=True)
+    bar = tqdm(total=file_size, leave=False,
+               file=file, ascii=True, disable=disable)
     with bar as bar, open(path, 'rb') as f:
         while True:
             chunk = f.read(chunk_size)
@@ -42,7 +50,7 @@ def gethash(ctx, path, *, chunk_size=DEFAULT_CHUNK_SIZE):
 def generate_hash(ctx, patterns, *, suffix='.sha'):
     for pattern in patterns:
         for path in map(os.path.normpath, iglob(pattern)):
-            hash_value = gethash(ctx.copy(), path)
+            hash_value = calcualte_hash(ctx.copy(), path)
             hash_line = fhash(hash_value, path)
             hash_path = path + suffix
             with open(hash_path, 'w', encoding='utf-8') as f:
@@ -57,7 +65,7 @@ def check_hash(ctx, patterns):
             with open(hash_path, 'r', encoding='utf-8') as f:
                 hash_line = f.read()
             hash_value, path = phash(hash_line)
-            current_hash_value = gethash(ctx.copy(), path)
+            current_hash_value = calcualte_hash(ctx.copy(), path)
 
             if compare_digest(hash_value, current_hash_value):
                 click.secho('[SUCCESS] {}'.format(path), fg='green')
