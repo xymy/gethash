acc95c3797d4126b3ddf8fa6e78ae262d6d8d871
xymy <thyfan@163.com>
2020-07-01 11:00:30 +0800

    Refactor

--- src/gethash/_core.py
+++ src/gethash/_core.py
@@ -1,30 +1,27 @@
 import os
 import re
-import sys
-from glob import iglob
-from hmac import compare_digest
 from os import PathLike
 from typing import ByteString, Tuple
 
-import click
 from tqdm import tqdm
 
 _CHUNK_SIZE = 0x100000
+_HL_PAT = re.compile(r'([0-9a-fA-F]+) \*(.+)')
 
 
-def calculate_hash(
-    hash_context_prototype,
-    path: PathLike,
+def calc_hash(
+    ctx_proto,
+    path,
     *,
     chunk_size=_CHUNK_SIZE,
     **tqdm_args
 ) -> bytes:
     """Calculate the hash value of `path` using the copy of given hash context
-    prototype.
+    prototype `ctx_proto`.
 
     A tqdm progressbar is also available.
     """
-    ctx = hash_context_prototype.copy()
+    ctx = ctx_proto.copy()
     file_size = os.path.getsize(path)
     leave = tqdm_args.pop('leave', False)
     ascii = tqdm_args.pop('ascii', True)
@@ -46,115 +43,8 @@ def fhash(hash_value: ByteString, path: PathLike) -> str:
 
 def phash(hash_line: str) -> Tuple[bytes, str]:
     """Parse `hash_line` to `hash_value` and `path`."""
-    m = re.match(r'([0-9a-fA-F]+) \*(.+)', hash_line)
+    m = _HL_PAT.match(hash_line)
     if m is None:
         raise ValueError('unexpected hash line')
     hash_value, path = m.groups()
     return bytes.fromhex(hash_value), path
-
-
-class MissingFile(FileNotFoundError):
-    """For the filename listed in checksum file but the file not exists."""
-
-
-def _echo_error(path, exc):
-    message = '[ERROR] {}\n\t{}: {}'.format(path, type(exc).__name__, exc)
-    click.secho(message, err=True, fg='red')
-
-
-class GetHash(object):
-    def __init__(self, ctx, *, file=sys.stdout, suffix='.sha', **kwargs):
-        self.ctx = ctx
-        self.file = file
-        self.suffix = suffix
-        self.tqdm_args = {'file': file, **kwargs}
-
-    def generate_hash_line(self, path):
-        ctx = self.ctx.copy()
-        hash_value = calculate_hash(ctx, path, **self.tqdm_args)
-        return fhash(hash_value, path)
-
-    def check_hash_line(self, hash_line):
-        hash_value, path = phash(hash_line)
-
-        # If we cannot find the file listed in hash line, raise `MissingFile`.
-        try:
-            ctx = self.ctx.copy()
-            current_hash_value = calculate_hash(ctx, path, **self.tqdm_args)
-        except FileNotFoundError:
-            raise MissingFile(path)
-
-        is_ok = compare_digest(hash_value, current_hash_value)
-        return is_ok, path
-
-
-def generate_hash_line(ctx, path, **tqdm_args):
-    hash_value = calculate_hash(ctx.copy(), path, **tqdm_args)
-    return fhash(hash_value, path)
-
-
-def generate_hash(ctx, patterns, *, file=sys.stdout, suffix='.sha'):
-    for pattern in patterns:
-        for path in map(os.path.normpath, iglob(pattern)):
-            try:
-                hash_line = generate_hash_line(ctx, path, file=file)
-                hash_path = path + suffix
-                with open(hash_path, 'w', encoding='utf-8') as f:
-                    f.write(hash_line)
-            except Exception as e:
-                _echo_error(path, e)
-            else:
-                click.echo(hash_line, file=file, nl=False)
-
-
-def check_hash_line(ctx, hash_line, **tqdm_args):
-    hash_value, path = phash(hash_line)
-
-    # If we cannot find the file listed in hash line, raise `MissingFile`.
-    try:
-        current_hash_value = calculate_hash(ctx.copy(), path, **tqdm_args)
-    except FileNotFoundError:
-        raise MissingFile(path)
-
-    is_ok = compare_digest(hash_value, current_hash_value)
-    return is_ok, path
-
-
-def _check_hash(ctx, hash_line, hash_path, *, file=sys.stdout):
-    try:
-        is_ok, path = check_hash_line(ctx, hash_line, file=file)
-    except Exception as e:
-        _echo_error(hash_path, e)
-    else:
-        if is_ok:
-            click.secho('[SUCCESS] {}'.format(path), file=file, fg='green')
-        else:
-            click.secho('[FAILURE] {}'.format(path), file=file, fg='red')
-
-
-def check_hash(ctx, patterns, *, file=sys.stdout):
-    for pattern in patterns:
-        for hash_path in map(os.path.normpath, iglob(pattern)):
-            try:
-                with open(hash_path, 'r', encoding='utf-8') as f:
-                    for hash_line in f:
-                        if hash_line.isspace():
-                            continue
-                        _check_hash(ctx, hash_line, hash_path, file=file)
-            except Exception as e:
-                _echo_error(hash_path, e)
-
-
-def script_main(command, ctx, suffix, check, files, **kwargs):
-    # When no argument, print help.
-    if not files:
-        sys.argv.append('--help')
-        command()
-
-    no_stdout = kwargs.pop('no_stdout', False)
-    file = open(os.devnull, 'w') if no_stdout else sys.stdout
-
-    if check:
-        check_hash(ctx, files, file=file)
-    else:
-        generate_hash(ctx, files, file=file, suffix=suffix)
---
+++ src/gethash/_script.py
@@ -0,0 +1,117 @@
+import os
+import sys
+from glob import iglob
+from hmac import compare_digest
+
+import click
+
+from ._core import calc_hash, fhash, phash
+
+
+class MissingFile(FileNotFoundError):
+    """For the filename listed in checksum file but the file not exists."""
+
+
+class GetHash(object):
+    """Provide script template."""
+
+    def __init__(self, ctx, *, suffix='.sha', **kwargs):
+        self.ctx = ctx
+        self.suffix = suffix
+        self.file = kwargs.pop('file', sys.stdout)
+        self.errfile = kwargs.pop('errfile', sys.stderr)
+        self.no_glob = kwargs.pop('no_glob', False)
+        self.tqdm_args = {'file': self.file, **kwargs}
+
+    def echo(self, msg, **kwargs):
+        click.echo(msg, file=self.file, **kwargs)
+
+    def secho(self, msg, **kwargs):
+        click.secho(msg, file=self.file, **kwargs)
+
+    def echo_error(self, path, exc):
+        message = '[ERROR] {}\n\t{}: {}'.format(path, type(exc).__name__, exc)
+        click.secho(message, file=self.errfile, fg='red')
+
+    def ghl(self, path):
+        """Generate hash line."""
+        hash_value = calc_hash(self.ctx, path, **self.tqdm_args)
+        return fhash(hash_value, path)
+
+    def chl(self, hash_line):
+        """Check hash line."""
+        hash_value, path = phash(hash_line)
+
+        # If we cannot find the file listed in hash line, raise `MissingFile`.
+        try:
+            curr_hash_value = calc_hash(self.ctx, path, **self.tqdm_args)
+        except FileNotFoundError:
+            raise MissingFile(path)
+
+        is_ok = compare_digest(hash_value, curr_hash_value)
+        return is_ok, path
+
+    def glob(self, patterns):
+        if self.no_glob:
+            def glob_func(pattern):
+                yield pattern
+        else:
+            glob_func = iglob
+
+        for pattern in patterns:
+            for path in map(os.path.normpath, glob_func(pattern)):
+                yield path
+
+    def generate_hash(self, patterns):
+        for path in self.glob(patterns):
+            try:
+                hash_line = self.ghl(path)
+                hash_path = path + self.suffix
+                with open(hash_path, 'w', encoding='utf-8') as f:
+                    f.write(hash_line)
+            except Exception as e:
+                self.echo_error(path, e)
+            else:
+                self.echo(hash_line, nl=False)
+
+    def _check_hash(self, hash_line, hash_path):
+        try:
+            is_ok, path = self.chl(hash_line)
+        except Exception as e:
+            self.echo_error(hash_path, e)
+        else:
+            if is_ok:
+                self.secho('[SUCCESS] {}'.format(path), fg='green')
+            else:
+                self.secho('[FAILURE] {}'.format(path), fg='red')
+
+    def check_hash(self, patterns):
+        for hash_path in self.glob(patterns):
+            try:
+                with open(hash_path, 'r', encoding='utf-8') as f:
+                    for hash_line in f:
+                        if hash_line.isspace():
+                            continue
+                        self._check_hash(hash_line, hash_path)
+            except Exception as e:
+                self.echo_error(hash_path, e)
+
+
+def script_main(command, ctx, suffix, check, files, **kwargs):
+    # When no argument, print help.
+    if not files:
+        sys.argv.append('--help')
+        command()
+
+    no_stdout = kwargs.pop('no_stdout', False)
+    file = open(os.devnull, 'w') if no_stdout else sys.stdout
+    no_stderr = kwargs.pop('no_stderr', False)
+    errfile = open(os.devnull, 'w') if no_stderr else sys.stderr
+
+    args = {'file': file, 'errfile': errfile}
+    gh = GetHash(ctx, suffix=suffix, **args)
+
+    if check:
+        gh.check_hash(files)
+    else:
+        gh.generate_hash(files)
