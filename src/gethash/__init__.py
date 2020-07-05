ca769f22576bb1edda0e3d3da4d3fb271b8eb6f5
xymy <thyfan@163.com>
2020-07-05 16:36:42 +0800

    Add __all__

--- src/gethash/__init__.py
+++ src/gethash/__init__.py
@@ -2,6 +2,9 @@ __project__ = 'gethash'
 __version__ = '0.9'
 __author__ = 'xymy'
 
+__all__ = ['ParseHashLineError', 'ParseHashLineError',
+           'calc_hash', 'fhl', 'phl', 'ghl', 'chl']
+
 from ._core import (    # noqa: F401
     CheckHashLineError,
     ParseHashLineError,
