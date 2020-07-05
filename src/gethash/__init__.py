14c789a34531a4d918787f0ff07eb0289b623cc7
xymy <thyfan@163.com>
2020-07-05 16:35:07 +0800

    Export APIs

--- src/gethash/__init__.py
+++ src/gethash/__init__.py
@@ -1,3 +1,13 @@
 __project__ = 'gethash'
-__version__ = '0.8'
+__version__ = '0.9'
 __author__ = 'xymy'
+
+from ._core import (    # noqa: F401
+    CheckHashLineError,
+    ParseHashLineError,
+    calc_hash,
+    chl,
+    fhl,
+    ghl,
+    phl
+)
