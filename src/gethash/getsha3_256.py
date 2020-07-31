ba9dc8dec80d4a100fc945bf76dda0f9ed6c2523
xymy <thyfan@163.com>
2020-07-31 08:51:43 +0800

    Add SHA3

---
+++ src/gethash/getsha3_256.py
@@ -0,0 +1,14 @@
+from ._script import gethashcli, script_main
+
+NAME = 'SHA3-256'
+SUFFIX = '.sha3_256'
+
+
+@gethashcli(NAME)
+def main(check, files, **kwargs):
+    from hashlib import sha3_256 as H
+    script_main(H(), SUFFIX, check, files, **kwargs)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
---
+++ src/gethash/getsha3_512.py
@@ -0,0 +1,14 @@
+from ._script import gethashcli, script_main
+
+NAME = 'SHA3-512'
+SUFFIX = '.sha3_512'
+
+
+@gethashcli(NAME)
+def main(check, files, **kwargs):
+    from hashlib import sha3_512 as H
+    script_main(H(), SUFFIX, check, files, **kwargs)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
