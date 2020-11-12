2d84893a134937ab45ffda5a85640647f96b93db
xymy <thyfan@163.com>
2020-11-12 22:50:44 +0800

    Add color support

--- src/gethash/utils.py
+++ src/gethash/utils.py
@@ -1,5 +1,48 @@
 import glob
 import os
+import sys
+
+ANSI_COLORS_FORE = {
+    "black": "\x1b[30m",
+    "red": "\x1b[31m",
+    "green": "\x1b[32m",
+    "yellow": "\x1b[33m",
+    "blue": "\x1b[34m",
+    "magenta": "\x1b[35m",
+    "cyan": "\x1b[36m",
+    "white": "\x1b[37m",
+    "reset": "\x1b[39m",
+    "bright_black": "\x1b[90m",
+    "bright_red": "\x1b[91m",
+    "bright_green": "\x1b[92m",
+    "bright_yellow": "\x1b[93m",
+    "bright_blue": "\x1b[94m",
+    "bright_magenta": "\x1b[95m",
+    "bright_cyan": "\x1b[96m",
+    "bright_white": "\x1b[97m",
+}
+
+ANSI_COLORS_BACK = {
+    "black": "\x1b[40m",
+    "red": "\x1b[41m",
+    "green": "\x1b[42m",
+    "yellow": "\x1b[43m",
+    "blue": "\x1b[44m",
+    "magenta": "\x1b[45m",
+    "cyan": "\x1b[46m",
+    "white": "\x1b[47m",
+    "reset": "\x1b[49m",
+    "bright_black": "\x1b[100m",
+    "bright_red": "\x1b[101m",
+    "bright_green": "\x1b[102m",
+    "bright_yellow": "\x1b[103m",
+    "bright_blue": "\x1b[104m",
+    "bright_magenta": "\x1b[105m",
+    "bright_cyan": "\x1b[106m",
+    "bright_white": "\x1b[107m",
+}
+
+ANSI_RESET_ALL = "\033[0m"
 
 
 def glob_resolver(pathname, *, mode=0, recursive=False):
@@ -7,7 +50,7 @@ def glob_resolver(pathname, *, mode=0, recursive=False):
     if mode == 0:
         yield pathname
     elif mode == 1:
-        pathname = pathname.replace('[', glob.escape('['))
+        pathname = pathname.replace("[", glob.escape("["))
         yield from glob.iglob(pathname, recursive=recursive)
     elif mode == 2:
         yield from glob.iglob(pathname, recursive=recursive)
@@ -19,3 +62,32 @@ def glob_scanner(pathnames, *, mode=0, recursive=False):
     for pathname in pathnames:
         for path in glob_resolver(pathname, mode=mode, recursive=recursive):
             yield path
+
+
+def wrap_stream(stream, *, convert=None, strip=None, autoreset=False):
+    if os.name == "nt":
+        # Use lazy loading so that colorama is only required on Windows.
+        import colorama
+
+        wrapper = colorama.AnsiToWin32(
+            stream, convert=convert, strip=strip, autoreset=autoreset
+        )
+        if wrapper.should_wrap():
+            stream = wrapper.stream
+    return stream
+
+
+def cwrite(obj, *, file=sys.stdout, fg=None, bg=None):
+    if fg:
+        try:
+            obj = ANSI_COLORS_FORE[fg] + obj
+        except KeyError:
+            raise ValueError("invalid foreground color '{}'".format(fg))
+
+    if bg:
+        try:
+            obj = ANSI_COLORS_BACK[bg] + obj
+        except KeyError:
+            raise ValueError("invalid background color '{}'".format(bg))
+
+    file.write(obj + ANSI_RESET_ALL)
