b7ddeea0fb9cc8244b54ff9c344e9213a5fd4be9
xymy <thyfan@163.com>
2020-11-24 21:45:17 +0800

    Add _check_int_opt

--- src/gethash/utils/__init__.py
+++ src/gethash/utils/__init__.py
@@ -1,7 +1,17 @@
 def _check_int(obj, name):
     if not isinstance(obj, int):
         tname = type(obj).__name__
-        raise TypeError("{} must be a integer, not {}".format(name, tname))
+        raise TypeError("{} must be an integer, not {}".format(name, tname))
+
+
+def _check_int_opt(obj, name, default):
+    if obj is None:
+        return default
+
+    if not isinstance(obj, int):
+        tname = type(obj).__name__
+        raise TypeError("{} must be None or an integer, not {}".format(name, tname))
+    return obj
 
 
 def _check_str(obj, name):
