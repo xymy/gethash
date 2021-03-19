f10a9e082646da076c14e4e884644c7437c142cf
xymy <thyfan@163.com>
2021-03-19 20:59:57 +0800

    Add _check_bytearray & Adjust error msg

--- src/gethash/utils/__init__.py
+++ src/gethash/utils/__init__.py
@@ -1,7 +1,7 @@
 def _check_int(obj, name):
     if not isinstance(obj, int):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be an integer, not {tname}")
+        raise TypeError(f"{name} must be int, not {tname}")
 
 
 def _check_int_opt(obj, name, default=None):
@@ -10,14 +10,14 @@ def _check_int_opt(obj, name, default=None):
 
     if not isinstance(obj, int):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be None or an integer, not {tname}")
+        raise TypeError(f"{name} must be int or None, not {tname}")
     return obj
 
 
 def _check_str(obj, name):
     if not isinstance(obj, str):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be a string, not {tname}")
+        raise TypeError(f"{name} must be str, not {tname}")
 
 
 def _check_str_opt(obj, name, default=None):
@@ -26,14 +26,14 @@ def _check_str_opt(obj, name, default=None):
 
     if not isinstance(obj, str):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be None or a string, not {tname}")
+        raise TypeError(f"{name} must be str or None, not {tname}")
     return obj
 
 
 def _check_bytes(obj, name):
     if not isinstance(obj, (bytes, bytearray)):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be a byte string, not {tname}")
+        raise TypeError(f"{name} must be bytes or bytearray, not {tname}")
 
 
 def _check_bytes_opt(obj, name, default=None):
@@ -42,5 +42,21 @@ def _check_bytes_opt(obj, name, default=None):
 
     if not isinstance(obj, (bytes, bytearray)):
         tname = type(obj).__name__
-        raise TypeError(f"{name} must be None or a byte string, not {tname}")
+        raise TypeError(f"{name} must be bytes, bytearray or None, not {tname}")
+    return obj
+
+
+def _check_bytearray(obj, name):
+    if not isinstance(obj, bytearray):
+        tname = type(obj).__name__
+        raise TypeError(f"{name} must be bytearray, not {tname}")
+
+
+def _check_bytearray_opt(obj, name, default=None):
+    if obj is None:
+        return default
+
+    if not isinstance(obj, bytearray):
+        tname = type(obj).__name__
+        raise TypeError(f"{name} must be bytearray or None, not {tname}")
     return obj
