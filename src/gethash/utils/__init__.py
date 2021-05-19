e62a8d761e32a71f5f5e00fb3abf717ad75e4aa4
xymy <thyfan@163.com>
2021-05-19 17:54:48 +0800

    Improve type checkers

--- src/gethash/utils/__init__.py
+++ src/gethash/utils/__init__.py
@@ -11,13 +11,14 @@ __all__ = [
     "_check_bytes_opt",
     "_check_bytes_w",
     "_check_bytes_w_opt",
+    "_is_writable_memoryview",
 ]
 
 
 def _check_int(obj: object, name: str) -> None:
     if not isinstance(obj, int):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be int, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be int, not {t}")
 
 
 def _check_int_opt(obj: object, name: str, default: object = None) -> Any:
@@ -25,15 +26,15 @@ def _check_int_opt(obj: object, name: str, default: object = None) -> Any:
         return default
 
     if not isinstance(obj, int):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be int or None, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be int or None, not {t}")
     return obj
 
 
 def _check_float(obj: object, name: str) -> None:
     if not isinstance(obj, float):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be float, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be float, not {t}")
 
 
 def _check_float_opt(obj: object, name: str, default: object = None) -> Any:
@@ -41,15 +42,15 @@ def _check_float_opt(obj: object, name: str, default: object = None) -> Any:
         return default
 
     if not isinstance(obj, float):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be float or None, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be float or None, not {t}")
     return obj
 
 
 def _check_str(obj: object, name: str) -> None:
     if not isinstance(obj, str):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be str, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be str, not {t}")
 
 
 def _check_str_opt(obj: object, name: str, default: object = None) -> Any:
@@ -57,15 +58,15 @@ def _check_str_opt(obj: object, name: str, default: object = None) -> Any:
         return default
 
     if not isinstance(obj, str):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be str or None, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be str or None, not {t}")
     return obj
 
 
 def _check_bytes(obj: object, name: str) -> None:
     if not isinstance(obj, (bytes, bytearray, memoryview)):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be bytes-like, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be bytes-like, not {t}")
 
 
 def _check_bytes_opt(obj: object, name: str, default: object = None) -> Any:
@@ -73,15 +74,15 @@ def _check_bytes_opt(obj: object, name: str, default: object = None) -> Any:
         return default
 
     if not isinstance(obj, (bytes, bytearray, memoryview)):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be bytes-like or None, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be bytes-like or None, not {t}")
     return obj
 
 
 def _check_bytes_w(obj: object, name: str) -> None:
     if not (isinstance(obj, bytearray) or _is_writable_memoryview(obj)):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be writable bytes-like, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be writable bytes-like, not {t}")
 
 
 def _check_bytes_w_opt(obj: object, name: str, default: object = None) -> Any:
@@ -89,11 +90,10 @@ def _check_bytes_w_opt(obj: object, name: str, default: object = None) -> Any:
         return default
 
     if not (isinstance(obj, bytearray) or _is_writable_memoryview(obj)):
-        tname = type(obj).__name__
-        raise TypeError(f"{name} must be writable bytes-like or None, not {tname}")
+        t = type(obj).__name__
+        raise TypeError(f"{name} must be writable bytes-like or None, not {t}")
     return obj
 
 
 def _is_writable_memoryview(obj):
-    # TODO: _is_*
     return isinstance(obj, memoryview) and not obj.readonly
