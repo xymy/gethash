86d9697d4571ab74aa45bd6914c0d75e4171befe
xymy <thyfan@163.com>
2021-05-25 22:50:08 +0800

    Improve comments

--- src/gethash/cli/_codegen.py
+++ src/gethash/cli/_codegen.py
@@ -107,8 +107,8 @@ def _main(files, **kwargs):
 def main():
     # Since Click 8, glob patterns, user home directory and environment variables will
     # be expanded automatically on Windows, which causes unexpected behaviour. What's
-    # more, there is no way to disable expansion through quotation marks on command
-    # line. So we suppress this feature by passing arguments explicitly.
+    # more, there is no way to disable expansion through quotation marks on Windows
+    # command line. So we suppress this feature by passing arguments explicitly.
     return _main(sys.argv[1:])
 
 
