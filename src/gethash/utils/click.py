03b4a69adfe4769c58bad781e183f82c7e037961
xymy <thyfan@163.com>
2021-05-28 09:53:53 +0800

    Improve click

--- src/gethash/utils/click.py
+++ src/gethash/utils/click.py
@@ -5,11 +5,27 @@ import click
 
 class Command(click.Command):
     def main(self, args=None, *pargs, **kwargs):
-        args = args or sys.argv[1:]
+        if args is None:
+            args = sys.argv[1:]
         return super().main(args, *pargs, **kwargs)
 
 
 class MultiCommand(click.MultiCommand):
     def main(self, args=None, *pargs, **kwargs):
-        args = args or sys.argv[1:]
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
+
+
+class Group(click.Group):
+    def main(self, args=None, *pargs, **kwargs):
+        if args is None:
+            args = sys.argv[1:]
+        return super().main(args, *pargs, **kwargs)
+
+
+class CommandCollection(click.CommandCollection):
+    def main(self, args=None, *pargs, **kwargs):
+        if args is None:
+            args = sys.argv[1:]
         return super().main(args, *pargs, **kwargs)
