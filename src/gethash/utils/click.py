b5bcccbeed7026dde8565bb997dd1b486ab00d18
xymy <thyfan@163.com>
2021-05-31 20:23:49 +0800

    Keep compatibility with click 7.1 via override __init__ context_settings

--- src/gethash/utils/click.py
+++ src/gethash/utils/click.py
@@ -3,44 +3,57 @@ import sys
 import click
 
 
-class Context(click.Context):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("max_content_width", 120)
-        kwargs.setdefault("help_option_names", ["-h", "--help"])
-        super().__init__(*args, **kwargs)
-
-
 class Command(click.Command):
-    context_class = Context
+    def __init__(self, name, context_settings=None, **kwargs):
+        if context_settings is None:
+            context_settings = {}
+        context_settings.setdefault("max_content_width", 120)
+        context_settings.setdefault("help_option_names", ["-h", "--help"])
+        super().__init__(name, context_settings=context_settings, **kwargs)
 
-    def main(self, args=None, *pargs, **kwargs):
+    def main(self, args=None, **kwargs):
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, *pargs, **kwargs)
+        return super().main(args, **kwargs)
 
 
 class MultiCommand(click.MultiCommand):
-    context_class = Context
+    def __init__(self, name, context_settings=None, **kwargs):
+        if context_settings is None:
+            context_settings = {}
+        context_settings.setdefault("max_content_width", 120)
+        context_settings.setdefault("help_option_names", ["-h", "--help"])
+        super().__init__(name, context_settings=context_settings, **kwargs)
 
-    def main(self, args=None, *pargs, **kwargs):
+    def main(self, args=None, **kwargs):
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, *pargs, **kwargs)
+        return super().main(args, **kwargs)
 
 
 class Group(click.Group):
-    context_class = Context
+    def __init__(self, name, context_settings=None, **kwargs):
+        if context_settings is None:
+            context_settings = {}
+        context_settings.setdefault("max_content_width", 120)
+        context_settings.setdefault("help_option_names", ["-h", "--help"])
+        super().__init__(name, context_settings=context_settings, **kwargs)
 
-    def main(self, args=None, *pargs, **kwargs):
+    def main(self, args=None, **kwargs):
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, *pargs, **kwargs)
+        return super().main(args, **kwargs)
 
 
 class CommandCollection(click.CommandCollection):
-    context_class = Context
+    def __init__(self, name, context_settings=None, **kwargs):
+        if context_settings is None:
+            context_settings = {}
+        context_settings.setdefault("max_content_width", 120)
+        context_settings.setdefault("help_option_names", ["-h", "--help"])
+        super().__init__(name, context_settings=context_settings, **kwargs)
 
-    def main(self, args=None, *pargs, **kwargs):
+    def main(self, args=None, **kwargs):
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, *pargs, **kwargs)
+        return super().main(args, **kwargs)
