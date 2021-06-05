809057206d32d2cecda9ce7590994a7f6278a4db
xymy <thyfan@163.com>
2021-06-05 15:08:05 +0800

    Add _configure_context_settings & type hint

--- src/gethash/utils/click.py
+++ src/gethash/utils/click.py
@@ -1,15 +1,20 @@
 import sys
+from typing import Any, Dict, Optional
 
 import click
 
 
+def _configure_context_settings(attrs: Dict[str, Any]) -> None:
+    context_settings = attrs.pop("context_settings", {})
+    context_settings.setdefault("max_content_width", 120)
+    context_settings.setdefault("help_option_names", ["-h", "--help"])
+    attrs["context_settings"] = context_settings
+
+
 class Command(click.Command):
-    def __init__(self, name, context_settings=None, **kwargs):
-        if context_settings is None:
-            context_settings = {}
-        context_settings.setdefault("max_content_width", 120)
-        context_settings.setdefault("help_option_names", ["-h", "--help"])
-        super().__init__(name, context_settings=context_settings, **kwargs)
+    def __init__(self, name: Optional[str], **attrs: Any) -> None:
+        _configure_context_settings(attrs)
+        super().__init__(name, **attrs)
 
     def main(self, args=None, **kwargs):
         if args is None:
@@ -18,12 +23,9 @@ class Command(click.Command):
 
 
 class MultiCommand(click.MultiCommand):
-    def __init__(self, name, context_settings=None, **kwargs):
-        if context_settings is None:
-            context_settings = {}
-        context_settings.setdefault("max_content_width", 120)
-        context_settings.setdefault("help_option_names", ["-h", "--help"])
-        super().__init__(name, context_settings=context_settings, **kwargs)
+    def __init__(self, name: Optional[str], **attrs: Any) -> None:
+        _configure_context_settings(attrs)
+        super().__init__(name, **attrs)
 
     def main(self, args=None, **kwargs):
         if args is None:
@@ -32,12 +34,9 @@ class MultiCommand(click.MultiCommand):
 
 
 class Group(click.Group):
-    def __init__(self, name, context_settings=None, **kwargs):
-        if context_settings is None:
-            context_settings = {}
-        context_settings.setdefault("max_content_width", 120)
-        context_settings.setdefault("help_option_names", ["-h", "--help"])
-        super().__init__(name, context_settings=context_settings, **kwargs)
+    def __init__(self, name: Optional[str], **attrs: Any) -> None:
+        _configure_context_settings(attrs)
+        super().__init__(name, **attrs)
 
     def main(self, args=None, **kwargs):
         if args is None:
@@ -46,12 +45,9 @@ class Group(click.Group):
 
 
 class CommandCollection(click.CommandCollection):
-    def __init__(self, name, context_settings=None, **kwargs):
-        if context_settings is None:
-            context_settings = {}
-        context_settings.setdefault("max_content_width", 120)
-        context_settings.setdefault("help_option_names", ["-h", "--help"])
-        super().__init__(name, context_settings=context_settings, **kwargs)
+    def __init__(self, name: Optional[str], **attrs: Any) -> None:
+        _configure_context_settings(attrs)
+        super().__init__(name, **attrs)
 
     def main(self, args=None, **kwargs):
         if args is None:
