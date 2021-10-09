aa2ffee82d7308139f8518be7460f3c3f6bc8cdd
xymy <thyfan@163.com>
2021-10-09 21:08:54 +0800

    Update click

--- src/gethash/utils/click.py
+++ src/gethash/utils/click.py
@@ -1,55 +1,40 @@
 import sys
-from typing import Any, Dict, Optional
+from typing import Any, Optional, Sequence
 
 import click
 
 
-def _configure_context_settings(attrs: Dict[str, Any]) -> None:
-    context_settings = attrs.pop("context_settings", {})
-    context_settings.setdefault("max_content_width", 120)
-    context_settings.setdefault("help_option_names", ["-h", "--help"])
-    attrs["context_settings"] = context_settings
-
-
 class Command(click.Command):
-    def __init__(self, name: Optional[str], **attrs: Any) -> None:
-        _configure_context_settings(attrs)
-        super().__init__(name, **attrs)
-
-    def main(self, args=None, **kwargs):
+    def main(
+        self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any
+    ) -> Any:
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, **kwargs)
+        return super().main(args, *pargs, **kwargs)
 
 
 class MultiCommand(click.MultiCommand):
-    def __init__(self, name: Optional[str], **attrs: Any) -> None:
-        _configure_context_settings(attrs)
-        super().__init__(name, **attrs)
-
-    def main(self, args=None, **kwargs):
+    def main(
+        self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any
+    ) -> Any:
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, **kwargs)
+        return super().main(args, *pargs, **kwargs)
 
 
 class Group(click.Group):
-    def __init__(self, name: Optional[str], **attrs: Any) -> None:
-        _configure_context_settings(attrs)
-        super().__init__(name, **attrs)
-
-    def main(self, args=None, **kwargs):
+    def main(
+        self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any
+    ) -> Any:
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, **kwargs)
+        return super().main(args, *pargs, **kwargs)
 
 
 class CommandCollection(click.CommandCollection):
-    def __init__(self, name: Optional[str], **attrs: Any) -> None:
-        _configure_context_settings(attrs)
-        super().__init__(name, **attrs)
-
-    def main(self, args=None, **kwargs):
+    def main(
+        self, args: Optional[Sequence[str]] = None, *pargs: Any, **kwargs: Any
+    ) -> Any:
         if args is None:
             args = sys.argv[1:]
-        return super().main(args, **kwargs)
+        return super().main(args, *pargs, **kwargs)
