fb38a7d797ea9e15278f2765968f8d1c15ba0f52
xymy <thyfan@163.com>
2022-11-07 11:09:23 +0800

    Fix from yesqa

--- src/gethash/_compat.py
+++ src/gethash/_compat.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.version_info[:2] < (3, 10):
-    from importlib_metadata import entry_points  # noqa
+    from importlib_metadata import entry_points
 else:
     from importlib.metadata import entry_points  # noqa
