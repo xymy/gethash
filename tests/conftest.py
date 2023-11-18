5bcf4b0032d642a958b64efed65bad3dbc72c3b6
xymy <thyfan@163.com>
2023-11-18 21:00:39 +0800

    Remove unused code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 from collections.abc import Generator
-from pathlib import Path
 
 import pytest
 from pytest import TempPathFactory
@@ -14,38 +13,3 @@ def vectors(tmp_path_factory: TempPathFactory) -> Generator[Vectors, None, None]
     vectors.init_data_files()
     yield vectors
     vectors.finalize_data_files()
-
-
-@pytest.fixture(scope="module")
-def root() -> Path:
-    return Path(__file__).parent / "data"
-
-
-@pytest.fixture(scope="module")
-def foo_txt_path(root: Path) -> Path:
-    return root / "foo.txt"
-
-
-@pytest.fixture(scope="module")
-def foo_txt_sha256_path(root: Path) -> Path:
-    return root / "foo.txt.sha256"
-
-
-@pytest.fixture(scope="module")
-def foo_txt_a_sha256_path(root: Path) -> Path:
-    return root / "foo.txt.a.sha256"
-
-
-@pytest.fixture(scope="module")
-def foo_zip_path(root: Path) -> Path:
-    return root / "foo.zip"
-
-
-@pytest.fixture(scope="module")
-def foo_zip_sha256_path(root: Path) -> Path:
-    return root / "foo.zip.sha256"
-
-
-@pytest.fixture(scope="module")
-def foo_zip_sha256_a_path(root: Path) -> Path:
-    return root / "foo.zip.a.sha256"
